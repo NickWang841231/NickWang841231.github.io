b14a0101eea39ae074a89ee09c9835a443bdc773
Nick <m0935716520@gmail.com>
2021-07-31 11:35:20 +0800

    add hw4 lol

---
+++ JS作業/homework4/Modules/helpers.js
@@ -0,0 +1,19 @@
+function $g (cssselector) {
+    // 單筆 node
+    if(cssselector.includes('#')) {
+        return document.querySelector(cssselector);
+    }
+    
+    // 多筆 nodelist
+    let nodelist = document.querySelectorAll(cssselector);
+
+    return nodelist.length == 1 ? nodelist[0] : nodelist ; 
+
+}
+
+function $c(element) {
+    return document.createElement(element);
+}
+
+export {$g ,$c};
+
